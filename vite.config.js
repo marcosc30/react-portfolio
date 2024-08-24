7917397f050d497ddb31af9e10e86254dabd56bf
marcosc30 <marcos_carmona@brown.edu>
2024-08-24 21:11:41 +0200

    added react-vite to vit-config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // plugins: [react(), 'vite-plugin-react-svg'],
-  plugins: [react()],
+  plugins: [react()], 
+  base: '/react-vite/',
   css: {
     modules: {
       localsConvention: 'camelCase'
