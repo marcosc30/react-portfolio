5f48b565a24f313c3724d38c260134322baeeaa7
marcosc30 <marcos_carmona@brown.edu>
2024-08-24 21:06:53 +0200

    Fixed package.json

--- src/main.jsx
+++ src/main.jsx
@@ -8,5 +8,5 @@ import "@fontsource/outfit"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), 'vite-plugin-react-svg'],
+  // plugins: [react(), 'vite-plugin-react-svg'],
+  plugins: [react()],
   css: {
     modules: {
       localsConvention: 'camelCase'
