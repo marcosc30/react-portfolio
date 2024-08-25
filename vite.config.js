5e25658c39ccb0982d0a552ad7ef8d57d3a6c6bf
marcosc30 <marcos_carmona@brown.edu>
2024-08-25 20:06:39 +0200

    another attempt

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // plugins: [react(), 'vite-plugin-react-svg'],
-  plugins: [react()], 
-  esbuild: {
-    jsxInject: `import React from 'react'`,  // Automatically import React where JSX is used
-    loader: 'jsx',  // Ensure the ESBuild loader knows to handle JSX syntax
-  },
+  plugins: [react()],
+  base: '/react-vite/',
   css: {
     modules: {
       localsConvention: 'camelCase'
