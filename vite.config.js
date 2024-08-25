8bf3de28b2a2d277b4aeb003cc7ec9794d90f820
marcosc30 <marcos_carmona@brown.edu>
2024-08-25 20:47:53 +0200

    rebuilt with react-portfolio base

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [react(), 'vite-plugin-react-svg'],
+  //plugins: [react(), '@vitejs/plugin-react-refresh'],
   plugins: [react()],
-  base: '/',
+  base: '/react-portfolio/',
   css: {
     modules: {
       localsConvention: 'camelCase'
